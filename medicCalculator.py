f1ff5f8d13cbb0a5fb42d307c179083b0e333b0d
Kmetro12 <63415269+Kmetro12@users.noreply.github.com>
2021-07-13 22:10:27 -0400

    Add files via upload

--- medicCalculator.py
+++ medicCalculator.py
@@ -9,9 +9,13 @@ from kivy.uix.label import Label
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.button import Button
 from kivy.graphics import * 
+#from matplotlib.backend_bases import button_press_handler
 from kivy.uix.textinput import TextInput
 
+from kivy.core.window import Window #ssetting the window size in kivy 
 
+Window.clearcolor =(0,0,0,0)#setting the color of the window
+Window.size=(360,600) # setting the window size 
 '''
 class widget(Widget): #A class to inherit widgets 
     pass
@@ -62,7 +66,7 @@ class medCalcNight(RelativeLayout):#the medcalc nightmode class
         layoutHome = RelativeLayout() #creating the innerlayout
         
         lblDisclaimer = (Label(text="This application is intended to check the provides math. \nIf any discrepancies" +
-                            "arise refer to providers hand-written \nmath, and re-check ALL calculations. \nDo you agree to these terms of serive?",pos_hint={'center_x':0.6, 'center_y':0.8} )
+                            "arise refer to providers \nhand-written math, and re-check ALL calculations. \nDo you agree to these terms of serive?",pos_hint={'center_x':0.6, 'center_y':0.8} )
                               )#
         layoutHome.add_widget(lblDisclaimer)
         
@@ -93,27 +97,27 @@ class medCalcNight(RelativeLayout):#the medcalc nightmode class
             layoutHome.add_widget(lblHomePageTest)
             
             #adding buttons to problem types 
-            btnSupply = Button(text="Supply per 1mL",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.6})
+            btnSupply = Button(text="Supply per 1mL",size_hint=(0.4,0.1), pos_hint={'x':0.32, 'y':0.6})
             btnSupply.bind(on_press=Supply)#binding the button to a function 
             layoutHome.add_widget(btnSupply)
             
-            btnIvp = Button(text="IV Push",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.5})
+            btnIvp = Button(text="IV Push",size_hint=(0.4,0.1), pos_hint={'x':0.32, 'y':0.5})
             btnIvp.bind(on_press=IvPush)
             layoutHome.add_widget(btnIvp)
             
-            btnIvd = Button(text="IV Drip Problem",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.4} )
+            btnIvd = Button(text="IV Drip Problem",size_hint=(0.4,0.1), pos_hint={'x':0.32, 'y':0.4} )
             btnIvd.bind(on_press=IvDrip)
             layoutHome.add_widget(btnIvd)
             
-            btnDopa = Button(text="Dopamine Problem",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.3})
+            btnDopa = Button(text="Dopamine Problem",size_hint=(0.4,0.1), pos_hint={'x':0.32, 'y':0.3})
             btnDopa.bind(on_press=Dopamine)
             layoutHome.add_widget(btnDopa)
             
-            btnConvert = Button(text="Conversion",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.2})
+            btnConvert = Button(text="Conversion",size_hint=(0.4,0.1), pos_hint={'x':0.32, 'y':0.2})
             btnConvert.bind(on_press=Conversion)
             layoutHome.add_widget(btnConvert)
             
-            btnSettings = Button(text="Settings",size_hint=(0.3,0.1),pos_hint={'x':0.32, 'y':0.1})
+            btnSettings = Button(text="Settings",size_hint=(0.4,0.1),pos_hint={'x':0.32, 'y':0.1})
             btnSettings.bind(on_press=Settings)
             layoutHome.add_widget(btnSettings)
         #---------------------------------- 
@@ -125,26 +129,25 @@ class medCalcNight(RelativeLayout):#the medcalc nightmode class
         def Supply(self):
             
             clearScreen() #clearing the screen to place supply problem, and title label   
-            result = str("") #creating a string variable so result can then be placed in text box 
             
             lblSupply = Label(text="Find supply per 1mL",size_hint=(0.3,0.1), pos_hint={'x':0.32, 'y':0.7})
             layoutHome.add_widget(lblSupply)
             
             #creating buttons for supply 
-            lblMgs = Label(text="Enter milligrams (Mgs): ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.6})
+            lblMgs = Label(text="Enter milligrams\n                (MG): ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.6})
             layoutHome.add_widget(lblMgs)
-            txtMgs = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.42, 'y':0.6}, multiline=False)
+            txtMgs = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.48, 'y':0.6}, multiline=False)
             layoutHome.add_widget(txtMgs)
             
-            lblMls = Label(text="Enter milliliters (Mls): ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.5})
+            lblMls = Label(text="Enter milliliters\n                 (mL): ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.5})
             layoutHome.add_widget(lblMls)
-            txtMls = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.42, 'y':0.5}, multiline=False)
+            txtMls = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.48, 'y':0.5}, multiline=False)
             layoutHome.add_widget(txtMls)
             
-            lblMgspMl = Label(text="Milligrams per 1 mL: ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.4})
+            lblMgspMl = Label(text="      Milligrams \n         per 1 mL: ",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.4})
             layoutHome.add_widget(lblMgspMl)
             
-            txtAnswerSupp = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.42, 'y':0.4}, multiline=False, disabled=True) #disabled sets text input to read only 
+            txtAnswerSupp = TextInput(size_hint=(0.3,0.1), pos_hint={'x':0.48, 'y':0.4}, multiline=False, disabled=True) #disabled sets text input to read only 
             layoutHome.add_widget(txtAnswerSupp)
             
             #adding buttons to return to homescreen and calculate problem 
@@ -164,11 +167,11 @@ class medCalcNight(RelativeLayout):#the medcalc nightmode class
                     answer = "ERROR" #if exception is thrown then give error for answer 
                     txtAnswerSupp.text = answer
              
-            btnReturnHome = Button(text="Return to Homescreen",size_hint=(0.3,0.1), pos_hint={'x':0.15, 'y':0.3})
+            btnReturnHome = Button(text="Return to Home",size_hint=(0.35,0.1), pos_hint={'x':0.18, 'y':0.3})
             btnReturnHome.bind(on_press=returnHome)
             layoutHome.add_widget(btnReturnHome)
             
-            btnCalculate = Button(text="Calculate",size_hint=(0.3,0.1), pos_hint={'x':0.45, 'y':0.3})
+            btnCalculate = Button(text="Calculate",size_hint=(0.3,0.1), pos_hint={'x':0.52, 'y':0.3})
             btnCalculate.bind(on_press=supplyCalc)
             layoutHome.add_widget(btnCalculate)
             
